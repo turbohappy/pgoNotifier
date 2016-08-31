8dc1e5b31b3d71b11b1834fc461a10bfcb9be96d
Brian Davis <turbohappy@gmail.com>
2016-08-31 04:44:25 -0400

    Better notification - Closes #4

--- app/src/main/java/pgo/gottacatchemall/Notifier.java
+++ app/src/main/java/pgo/gottacatchemall/Notifier.java
@@ -1,10 +1,14 @@
 package pgo.gottacatchemall;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
 import android.support.v4.app.NotificationCompat;
+import android.widget.RemoteViews;
 
 /**
  * Created by bdavis on 8/30/16.
@@ -29,19 +33,25 @@ public class Notifier {
 	}
 
 	private void showNotification(PokeNotification pokeNotification) {
-		// Create Notification using NotificationCompat.Builder
+
 		NotificationCompat.Builder builder = new NotificationCompat.Builder(this.context)
 				.setSmallIcon(R.drawable.ic_info_black_24dp)
 				.setContentTitle(pokeNotification.title)
-				.setTicker(pokeNotification.shortText)
-				.setContentText(pokeNotification.shortText)
 				.setContentIntent(PendingIntent.getActivity(this.context, 0, new Intent(), 0))
-				.setStyle(new NotificationCompat.BigPictureStyle()
-						.bigPicture(pokeNotification.bitmap)
-						.setSummaryText(pokeNotification.longText))
-				.setAutoCancel(false);
+				.setAutoCancel(false)
+				.setVibrate(new long[]{0, 1000, 500, 1000, 500, 1000})
+				.setLights(Color.RED, 3000, 3000)
+				.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
+
+		Notification notification = builder.build();
+
+		RemoteViews contentView = new RemoteViews(this.context.getPackageName(), R.layout.notification);
+		contentView.setImageViewBitmap(R.id.image, pokeNotification.bitmap);
+		contentView.setTextViewText(R.id.title, pokeNotification.title);
+		contentView.setTextViewText(R.id.text, pokeNotification.longText);
+		notification.bigContentView = contentView;
 
 		// Build Notification with Notification Manager
-		notificationManager.notify(NOTIFICATION_ID, builder.build());
+		notificationManager.notify(NOTIFICATION_ID, notification);
 	}
 }
--- app/src/main/java/pgo/gottacatchemall/PokeFilter.java
+++ app/src/main/java/pgo/gottacatchemall/PokeFilter.java
@@ -7,7 +7,7 @@ import java.util.List;
  * Created by bdavis on 8/23/16.
  */
 public class PokeFilter {
-	private static Integer[] goodIdsArr = {/*13, */   4,5,6,25,26,63,64,65,66,67,68,86,87,88,89,92,93,94,106,107,108,113,131,138,139,140,141,142,143,144,145,146,147,148,149 };
+	private static Integer[] goodIdsArr = {10,13,16,    4,5,6,25,26,63,64,65,66,67,68,86,87,88,89,92,93,94,106,107,108,113,131,138,139,140,141,142,143,144,145,146,147,148,149 };
 	private static List<Integer> goodIds = Arrays.asList(goodIdsArr);
 
 	public static boolean desiredPokemon(int id) {
